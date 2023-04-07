03e2217d093d404b6b9ee8e51b690bc9543df1af
David Munro <defmunro@gmail.com>
2023-04-07 21:22:49 +0100

    add register inputs

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -37,11 +37,19 @@ const Register = () => {
     }
   };
 
-  const bodyContent = <div className="flex flex-col gap-4">
-
-    <Heading title="Welcome to Airbnb" subtitle="Create an account!" /> 
-    <Input/>
-  </div>;
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
