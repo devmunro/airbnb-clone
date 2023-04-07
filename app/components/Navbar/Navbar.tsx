0da052136452ae789197ff54323669cd772c9705
David Munro <defmunro@gmail.com>
2023-04-07 13:51:07 +0100

    add tailwind css to container-navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import Container from "../Container";
 
-
 function Navbar() {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px"></div>
-      <Container></Container>
+      <Container>
+        <div className="flex flex-row items-center justify-between gap-3 md:gap-0"></div>
+      </Container>
     </div>
   );
 }
