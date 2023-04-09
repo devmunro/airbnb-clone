e376653cbae4f7d928d09e8a9b6b446c631a5042
David Munro <defmunro@gmail.com>
2023-04-09 22:05:10 +0100

    add logout and logged in menu

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ currentUser?: User | null
 
 function Navbar(props: NavbarProps) {
   const {currentUser} = props
-  console.log(currentUser)
+ 
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px">
@@ -20,8 +20,8 @@ function Navbar(props: NavbarProps) {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search/>
-            <UserMenu/>
-           {currentUser?.name}
+            <UserMenu currentUser={currentUser}/>
+        
           </div>
         </Container>
       </div>
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -5,8 +5,15 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from 'next-auth/react';
 
-export default function UserMenu() {
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
+export default function UserMenu(props: UserMenuProps) {
+  const { currentUser } = props;
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +30,7 @@ export default function UserMenu() {
           //   onClick
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
-          Airbnb your home
+          Welcome
         </div>
         <div
           onClick={toggleOpen}
@@ -39,8 +46,22 @@ export default function UserMenu() {
         <div className="absolute rounded-xl  w-[40vw] md:w-3/4 shadow-xl bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <>
-              <MenuItem onClick={loginModal.onOpen} label="Login" />
-              <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
+              {currentUser ? (
+                <>
+                  <MenuItem onClick label="My trips" />
+                  <MenuItem onClick label="My favourites" />
+                  <MenuItem onClick label="My reservations" />
+                  <MenuItem onClick label="Airbnb my home" />
+                  <hr/>
+                  <MenuItem onClick={() => signOut()} label="Logout" />
+                  <hr/>
+                </>
+              ) : (
+                <>
+                  <MenuItem onClick={loginModal.onOpen} label="Login" />
+                  <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
+                </>
+              )}
             </>
           </div>
         </div>
