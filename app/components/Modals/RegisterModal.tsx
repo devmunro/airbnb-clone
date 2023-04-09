4fbb63d8350036011f9f1d1695d33de8c68fb126
David Munro <defmunro@gmail.com>
2023-04-09 10:22:40 +0100

    add signup toggle

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -9,6 +9,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../Inputs/inputs";
 import {toast} from "react-hot-toast"
+import Button from "../Button";
 
 const Register = () => {
   const registerModal = useRegisterModal();
@@ -37,6 +38,7 @@ const Register = () => {
       setIsLoading(false);
     }
   };
+  
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -68,6 +70,45 @@ const Register = () => {
       
     </div>
   );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn('google')} 
+      />
+      <Button 
+        outline 
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            onClick={registerModal.onClose} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            > Log in</span>
+        </p>
+      </div>
+    </div>
+  )
+
+
   return (
     <Modal
       disabled={isLoading}
@@ -77,6 +118,7 @@ const Register = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
